79c7921cc6a496a2dd49f80b4c28d2e1bb0e0e79
smoozmy <smoozmy@yandex.ru>
2025-07-08 13:31:12 +0300

    Fix 3
     Убрал урлы в константы

--- pages/login_page.py
+++ pages/login_page.py
@@ -2,6 +2,7 @@ import allure
 from locators.login_page_locators import LoginPageLocators
 from locators.header_locators import HeaderLocators
 from pages.base_page import BasePage
+from src.urls import Urls
 
 
 class LoginPage(BasePage):
@@ -15,6 +16,4 @@ class LoginPage(BasePage):
     @allure.step("Проверка успешного входа")
     def assert_user_logged_in(self):
         self.wait_element_visible(HeaderLocators.LOGOUT_BUTTON)
-        url = self.driver.current_url
-        assert url == 'https://foodgram-frontend-1.prakticum-team.ru/recipes'
-        return self
+        assert self.driver.current_url == Urls.RECIPES
